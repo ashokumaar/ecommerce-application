c7dd5089d1c8f3f245967c29a5ef25ef5bcc6ad5
ashokkumarvg <veeraashok53@gmail.com>
2024-06-27 19:03:39 +0530

    changing homepageurl to just a dot for git repositoy and later for gh-pages change the homepage and just run deploy on local machine

--- set-homepage.js
+++ set-homepage.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const packageJson = require('./package.json');
+// const fs = require('fs');
+// const packageJson = require('./package.json');
 
-const homepageUrl = "https://ashokumaar.github.io/ecommerce-application";
-packageJson.homepage = homepageUrl;
+// const homepageUrl = "https://ashokumaar.github.io/ecommerce-application";
+// packageJson.homepage = homepageUrl;
 
-fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
+// fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
